f4c7829b8ca499f02e07b6fd627bd1c3617b929f
btoo <feihcsim824@gmail.com>
2019-04-04 12:12:58 -0400

    Add context (this) type and unwrap Promise from resolve type

--- index.ts
+++ index.ts
@@ -1,37 +1,30 @@
-interface NoverlapConfig {
-  hash?: (...args:any[]) => any
-  comparator?: (hash:any, existingKey:any) => boolean
+type ResolveType<T> = T extends Promise<infer R> ? R : T;
+type HashFunction<T, P extends any[]> = (this: T, ...args: P) => any
+
+interface NoverlapConfig<T, P extends any[]> {
+  hash?: HashFunction<T, P>
+  comparator?: (hash: any, existingKey: any) => boolean
   wait?: number
-  start?: (...args:any[]) => any
-  queue?: (...args:any[]) => any
-  beforeFinish?: (...args:any[]) => any
-  success?: (result:any, ...args:any[]) => any
-  fail?: (result:Error, ...args:any[]) => any
-  finish?: (result:any, ...args:any[]) => any
+  start?: (...args: any[]) => any
+  queue?: (...args: any[]) => any
+  beforeFinish?: (...args: any[]) => any
+  success?: (result: any, ...args: any[]) => any
+  fail?: (result: Error, ...args: any[]) => any
+  finish?: (result: any, ...args: any[]) => any
 };
 
-/**
- * prevent function invocations (both sync and async) from redundantly overlapping with each other.
- * note that this will cause the function's return value to be async
- * @param config - noverlap configs
- * @param config.hash - a hash function of the async function parameters
- * @param config.comparator - a function used to determine if an existing key in the map is to be considered the same as a hash
- * @param config.wait - a wait time that will start on each execution of an async function and reset with every overlapping execution
- * @param config.start - a callback that is provided with the wrapped function's parameters and invoked when the wrapped function's key is hashed into the map
- * @param config.queue - a callback that is provided with the wrapped function's parameters and invoked whenever an invocation of the wrapped function is recorded
- * @param config.beforeFinish - a callback that is provided with the wrapped function's parameters and invoked right before the wrapped function
- * @param config.success - a callback that is provided with the wrapped function's invocation's result and arguments and executed after the wrapped function is successfully called
- * @param config.fail - a callback that is provided with the wrapped function's invocation's error and arguments and executed after the wrapped function is erroneously called
- * @param config.finish - a callback that is provided with the wrapped function's invocation's result and arguments and executed after the wrapped function is called
- * @returns - an instantiation of noverlap, a wrapper function used to apply noverlap to any function
- */
-export default <FR extends (...args:any[]) => any>(config:(NoverlapConfig|((...args:any[]) => NoverlapConfig)) = {}) => <F extends FR>(fn:F) => {
-  const map = new Map();
+type NoverlapConfigProvision<T, P extends any[]> = NoverlapConfig<T, P> | ((...args: any[]) => NoverlapConfig<T, P>);
 
-  type Params = Parameters<F>;
-  type Return = ReturnType<F>;
+type ResolveReject<R> = [(value?: R | PromiseLike<R>) => void, (reason?: any) => void];
 
-  return async function(...args:Params) {
+const wrappedFunctionCloner = <F extends (...args: any[]) => any, T, P extends any[], R>(
+  config: NoverlapConfigProvision<T, P>, fn: F, map: Map<any, ResolveReject<R>[]>
+): ((this: T, ...args: P) => Promise<R>) => {
+  return async function(...args: P) {
+
+    const self = this;
+    type Context = typeof self;
+
     const {
       hash = undefined,
       comparator = undefined,
@@ -48,12 +41,9 @@ export default <FR extends (...args:any[]) => any>(config:(NoverlapConfig|((...a
 
     const w = typeof wait === 'number' ? wait : 420;
 
-    type ResolveReject = [(value?: Return|PromiseLike<Return>) => void, (reason?: any) => void];
-
-    return new Promise<Return>(async (...resrej:ResolveReject) => {
-
+    return new Promise<R>(async (...resrej: ResolveReject<R>) => {
       const key = typeof hash === 'function'
-        ? await hash(...args)
+        ? await hash.call(this, ...args)
         : args[0];
 
       let keyReference = false;
@@ -70,9 +60,11 @@ export default <FR extends (...args:any[]) => any>(config:(NoverlapConfig|((...a
         keyReference = key;
       }
 
-      let value:ResolveReject[];
+      let value: ResolveReject<R>[];
       if (keyReference) {
-        value = map.get(keyReference);
+        const resolveRejects = map.get(keyReference);
+        !resolveRejects && console.warn(`Function invocations not found despite the existence of invocation hash.`, 'Function:', fn, 'Hash:', key);
+        value = resolveRejects || [];
         value.push(resrej);
       } else {
         value = [resrej];
@@ -84,7 +76,7 @@ export default <FR extends (...args:any[]) => any>(config:(NoverlapConfig|((...a
 
       setTimeout(async () => {
         if (value && value[value.length - 1] === resrej) {
-          let result:any;
+          let result: any;
           try {
             typeof beforeFinish === 'function' && await beforeFinish(...args);
             result = await fn.call(this, ...args);
@@ -101,5 +93,31 @@ export default <FR extends (...args:any[]) => any>(config:(NoverlapConfig|((...a
         }
       }, w);
     });
-  };
+  }
+}
+
+type ContextOfWrappedFunction = ThisType<ReturnType<typeof wrappedFunctionCloner>> | void
+
+/**
+ * prevent function invocations (both sync and async) from redundantly overlapping with each other.
+ * note that this will cause the function's return value to be async
+ * @param config - noverlap configs
+ * @param config.hash - a hash function of the async function parameters
+ * @param config.comparator - a function used to determine if an existing key in the map is to be considered the same as a hash
+ * @param config.wait - a wait time that will start on each execution of an async function and reset with every overlapping execution
+ * @param config.start - a callback that is provided with the wrapped function's parameters and invoked when the wrapped function's key is hashed into the map
+ * @param config.queue - a callback that is provided with the wrapped function's parameters and invoked whenever an invocation of the wrapped function is recorded
+ * @param config.beforeFinish - a callback that is provided with the wrapped function's parameters and invoked right before the wrapped function
+ * @param config.success - a callback that is provided with the wrapped function's invocation's result and arguments and executed after the wrapped function is successfully called
+ * @param config.fail - a callback that is provided with the wrapped function's invocation's error and arguments and executed after the wrapped function is erroneously called
+ * @param config.finish - a callback that is provided with the wrapped function's invocation's result and arguments and executed after the wrapped function is called
+ * @returns - an instantiation of noverlap, a wrapper function used to apply noverlap to any function
+ */
+export default <FR extends (...args: any[]) => any>(config: NoverlapConfigProvision<ContextOfWrappedFunction, Parameters<FR>> = {}) => <F extends FR>(fn: F) => {
+  const map = new Map();
+
+  type Params = Parameters<F>;
+  type Return = ResolveType<ReturnType<F>>;
+
+  return wrappedFunctionCloner<F, ContextOfWrappedFunction, Params, Return>(config, fn, map);
 };
